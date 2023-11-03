4ded58a9eb6e9d2c44b7abe33805f461ec04354f
Kiril Morozov <kmorozov@minsait.com>
2023-11-03 10:04:17 +0100

    G Actions for testing

---
+++ src/test/test.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+
+class BaseTest(unittest.TestCase):
+    """Test Class"""
+
+    def test_initial(self):
+        self.assertAlmostEqual(1, 1)
+
+
+if __name__ == "__main__":
+    unittest.main()
